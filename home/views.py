5f20d10dff3f3b153256bd3b79116d4d5465cd49
Pranjal0512 <pranjal.barnwal051234@gmail.com>
2023-03-28 21:00:02 +0545

    functions added0

--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def blog_list(request):
+    return render(request,'blog_list.html')
+
+def product(request):
+    return render(request,'product.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
